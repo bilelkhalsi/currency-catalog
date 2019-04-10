fce34c22a64f1b0e2cc7e9c8902c4020f1c87a5a
bilel khalsi <bilel_khalsi@live.fr>
2019-04-11 00:31:43 +0200

    feat: add ngrx to manage app state

--- src/app/zipcode/app.zipcodes.component.ts
+++ src/app/zipcode/app.zipcodes.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-zipcodes',
-    templateUrl: './app.zipcodes.component.html'
+    templateUrl: './app.zipcodes.component.html',
+    styleUrls: ['./app.zipcodes.component.scss']
 })
 
 export class ZipCodesComponent implements OnInit {
